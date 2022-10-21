bf0427e5ae18aed9cda1bf4f9153c4b1b98aa05a
PMG <pattoyo01@gmail.com>
2022-10-21 12:39:36 -0300

    se cargan todos los componentes

--- proyectofinal/src/app/app.module.ts
+++ proyectofinal/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgCircleProgressModule } from 'ng-circle-progress';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { HardsoftComponent } from './componentes/hardsoft/hardsoft.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercadeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    ProyectosComponent,
+    HardsoftComponent,
+    FooterComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({ })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
